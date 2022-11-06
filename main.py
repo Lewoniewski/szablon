eecc979ff31a123c244e5f1764967a46ef111612
Lewoniewski <wlodzimierz.lewoniewski@gmail.com>
2022-11-06 15:33:11 +0100

    Update main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+#Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input(). Funkcja strip() mu
+wyraz=input()
+
+#W ramach GuHub Classroom warto dodatkowo użyć funkcję strip() - w celu usunięcia spacji oraz innych "spacjopodobnych" znaków z "głowy" i "ogona" (lewej i prawej części wyrazu).
+wyraz=wyraz.strip()
+
+#Wydruk na ekranie (w konsoli)
+print ('Ten wyraz został wprowadzony:', wyraz)
