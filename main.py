88fd6d84b2c13d1c333ed87a715d56a11ad743a7
Lewoniewski <wlodzimierz.lewoniewski@gmail.com>
2022-11-06 16:14:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Przykład otrzymania wartości wprowadzonej przy użyciu funkcji input().
 wyraz=input()
 
-#W celu poprawnego działania w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
+#W celu poprawnego działania kodu w ramach GitHub Classroom warto dodatkowo użyć funkcję strip()
 #To pozwoli na usunięcie spacji oraz innych "spacjopodobnych" znaków (tabulacja \t', przejście do nowej linii '\n' lub '\r' etc.) z "głowy" i "ogona" (lewej i prawej części wyrazu).
 wyraz=wyraz.strip()
 
